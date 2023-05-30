0fb0e4ecee82d3198ef7ee7f11c826cc2583e42f
juliovcf <52920962+juliovcf@users.noreply.github.com>
2023-05-30 19:44:15 +0200

    viewmodel

--- app/src/main/java/com/bambino/kingof3/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/bambino/kingof3/viewmodel/MainViewModel.kt
@@ -1,4 +1,31 @@
 package com.bambino.kingof3.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bambino.kingof3.data.database.PlayerRepository
+import com.bambino.kingof3.data.database.RuleRepository
+import com.bambino.kingof3.data.model.Player
+import com.bambino.kingof3.data.model.Rule
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val playerRepository: PlayerRepository, private val ruleRepository: RuleRepository) : ViewModel() {
+    val players: LiveData<List<Player>> = playerRepository.players
+    val activeRules: LiveData<List<Rule>> = ruleRepository.activeRules
+    val inactiveRules: LiveData<List<Rule>> = ruleRepository.inactiveRules
+
+    fun addRule(description: String, isActivated: Boolean) {
+        viewModelScope.launch {
+            ruleRepository.insertRule(Rule(0, description, isActivated))
+        }
+    }
+
+    fun toggleRuleActivation(rule: Rule) {
+        viewModelScope.launch {
+            rule.isActivated = !rule.isActivated
+            ruleRepository.updateRule(rule)
+        }
+    }
+
+    // Agrega métodos para manejar los lanzamientos de dados y determinar quién bebe, según las reglas de tu juego
 }
--- app/src/main/java/com/bambino/kingof3/viewmodel/SetupViewModel.kt
+++ app/src/main/java/com/bambino/kingof3/viewmodel/SetupViewModel.kt
@@ -1,4 +1,18 @@
 package com.bambino.kingof3.viewmodel
 
-class SetupViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bambino.kingof3.data.database.PlayerRepository
+import com.bambino.kingof3.data.model.Player
+import kotlinx.coroutines.launch
+
+class SetupViewModel(private val playerRepository: PlayerRepository) : ViewModel() {
+    val players: LiveData<List<Player>> = playerRepository.players
+
+    fun addPlayer(name: String) {
+        viewModelScope.launch {
+            playerRepository.insertPlayer(Player(0, name, false, 0, 0))
+        }
+    }
 }
